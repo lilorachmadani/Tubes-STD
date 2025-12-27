9b184793cf891a012de639219b7b2403f2633165
KiriHoshi <gevin.spratama@gmail.com>
2025-12-27 13:28:21 +0700

    Update Rent.cpp

---
+++ Rent.cpp
@@ -0,0 +1,19 @@
+#include "Rent.h"
+#include <iostream>
+using namespace std;
+
+Toko *firstToko = NULL;
+Barang *firstBarang = NULL;
+Relasi *firstRelasi = NULL;
+
+Toko *cariToko(int idToko)
+{
+    Toko *p = firstToko;
+    while (p != NULL)
+    {
+        if (p->idToko == idToko)
+            return p;
+        p = p->next;
+    }
+    return NULL;
+}
